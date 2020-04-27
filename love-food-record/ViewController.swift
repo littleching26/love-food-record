9176d307f740842d911926f789033e48a452b501
Clay <Clay.Lin@ibm.com>
2020-04-27 23:45:02 +0800

    Initial Commit

---
+++ love-food-record/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  love-food-record
+//
+//  Created by 林逸清 on 2020/4/27.
+//  Copyright © 2020 林逸清. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ love-food-recordTests/love_food_recordTests.swift
@@ -0,0 +1,34 @@
+//
+//  love_food_recordTests.swift
+//  love-food-recordTests
+//
+//  Created by 林逸清 on 2020/4/27.
+//  Copyright © 2020 林逸清. All rights reserved.
+//
+
+import XCTest
+@testable import love_food_record
+
+class love_food_recordTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
